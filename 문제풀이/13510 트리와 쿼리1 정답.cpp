1b924f4b0a1cd754ef90676ca4b64170a4dabe9c
KYS <dustn9401@naver.com>
2018-10-07 20:19:49 +0900

    파일 이름변경, 트리와쿼리1 정답!!

--- 문제풀이/13510 트리와 쿼리1 정답.cpp
+++ 문제풀이/13510 트리와 쿼리1 정답.cpp
@@ -5,18 +5,18 @@
 #include <ctime>
 using namespace std;
 int n, m, u[100001], v[100001], w[100001], level[100001], heavy[100001], divtree_size = 1;
-int starts[100001];
+int starts[100001], parents[100001];
 typedef struct pair<int, int> ii;
 vector<list<ii>> adj;
 vector<list<int>> chlds;
-vector<int> segtree, segtree_idx, idx_in_divtree, node_edge_idx, divtrees;
-int parents[100001];
+vector<int> segtree, segtree_idx, idx_in_divtree, node_edge_idx, divtrees, node_weights;
 void maketree(int cur, int bef) {
 	level[cur] = level[bef] + 1;
 	for (ii i : adj[cur]) {
 		if (bef == i.first) continue;
 		chlds[cur].push_back(i.first);
 		node_edge_idx[i.second] = i.first;
+		node_weights[i.first] = w[i.second];
 		maketree(i.first, cur);
 		heavy[cur] += heavy[i.first];
 	}
@@ -31,10 +31,8 @@ void HLD(int cur, int bef) {
 	for (int i : chlds[cur])
 		if (i != high)
 			starts[i] = i, HLD(i, cur);
-	if (high != 0) 
+	if (high != 0)
 		starts[high] = starts[cur], HLD(high, cur);
-	//if (cur == 1) return;
-	//if (starts[cur] == 0) starts[cur] = starts[bef];
 	divtrees[divtree_size] = cur;
 	idx_in_divtree[cur] = divtree_size++;
 }
@@ -47,14 +45,15 @@ int update(int st, int ed, int idx, int change, int cur) {
 		update(st, (st + ed) / 2, idx, change, cur * 2),
 		update((st + ed) / 2 + 1, ed, idx, change, cur * 2 + 1));
 }
-void make_segtree() {
-	for (int i = 1; i < divtree_size; i++)
-		update(1, divtree_size - 1, idx_in_divtree[node_edge_idx[i]], w[i], 1);
+int make_segtree(int l, int r, int idx) {
+	if (l == r) return segtree[idx] = node_weights[divtrees[l]];
+	return segtree[idx] = max(
+		make_segtree(l, (l + r) / 2, idx * 2),
+		make_segtree((l + r) / 2 + 1, r, idx * 2 + 1));
 }
 int small_query(int st, int ed, int l, int r, int cur) {
 	if (r < st || l > ed)
 		return 0;
-	//printf("(%d, %d, %d, %d, %d)\n", st, ed, l, r, cur);
 	if (l >= st && r <= ed)
 		return segtree[cur];
 	return max(
@@ -66,84 +65,41 @@ int query(int st, int ed) {
 	while (starts[st] != starts[ed]) {
 		if (heavy[starts[st]] > heavy[starts[ed]])
 			swap(st, ed);
-		//printf("st %d, ed %d, ret = %d\n", st, ed, ret);
 		ret = max(ret, small_query(idx_in_divtree[st], idx_in_divtree[starts[st]], 1, divtree_size - 1, 1));
 		st = parents[starts[st]];
 	}
-	//printf("st %d, ed %d, ret = %d\n", st, ed, ret);
 	if (heavy[st] > heavy[ed]) swap(st, ed);
 	return max(ret, small_query(idx_in_divtree[st], idx_in_divtree[ed] - 1, 1, divtree_size - 1, 1));
 }
 int main()
 {
-	FILE *fp = fopen("C:\\input.txt", "r");
-	fscanf(fp, "%d", &n);
+	scanf("%d", &n);
 	adj.resize(n + 1),
 		idx_in_divtree.resize(n + 1),
 		segtree_idx.assign(n + 1, -1),
 		node_edge_idx.resize(n + 1),
 		chlds.resize(n + 1),
 		divtrees.resize(n + 1),
-		segtree.resize(n * 4);
+		segtree.resize(n * 4),
+		node_weights.resize(n+1);
 	starts[0] = starts[1] = parents[0] = parents[1] = 1;
 	for (int i = 1; i < n; i++) {
-		fscanf(fp, "%d%d%d", u + i, v + i, w + i);
+		scanf("%d%d%d", u + i, v + i, w + i);
 		adj[v[i]].push_back({ u[i], i });
 		adj[u[i]].push_back({ v[i], i });
 	}
-
-	long start = clock();
 	maketree(1, 0);
-	long end = clock();
-	printf("maketree: %.3lf\n", (end - start) / (double)CLOCKS_PER_SEC);
-
-	start = clock();
 	HLD(1, 0);
-	end = clock();
-	printf("HLD: %.3lf\n", (end - start) / (double)CLOCKS_PER_SEC);
-
-	start = clock();
-	make_segtree();
-	end = clock();
-	printf("make segtree: %.3lf\n", (end - start) / (double)CLOCKS_PER_SEC);
-	//for (int i = 1; i < divtree_size; i++)printf("%d, ", divtrees[i]);
-	double quer = 0, updat = 0;
-	FILE *fp2 = fopen("C:\\output.txt", "r");
-	fscanf(fp, "%d", &m);
-	//scanf("%d", &m);
+	make_segtree(1, divtree_size - 1, 1);
+	scanf("%d", &m);
 	for (int i = 0; i < m; i++) {
 		int a, b, c;
-		fscanf(fp, "%d%d%d", &a, &b, &c);
-		//scanf("%d%d%d", &a, &b, &c);
-
-		if (a == 2) {
-			start = clock();
-			int res = query(b, c);
-			end = clock();
-			quer += ((end - start) / (double)CLOCKS_PER_SEC);
-
-			int tmp;
-			fscanf(fp2, "%d", &tmp);
-			if (res == tmp) printf("O");
-			else printf("X");
-			//printf("res = %d\n", res);
-		}
-		if (a == 1) {
-			start = clock();
-			int node = node_edge_idx[b];
-			int idx = idx_in_divtree[node];
-			update(1, divtree_size - 1, idx, c, 1);
-
-			end = clock();
-			updat += ((end - start) / (double)CLOCKS_PER_SEC);
-		}
+		scanf("%d%d%d", &a, &b, &c);
+		if (a == 2)
+			printf("%d\n", query(b, c));
+		if (a == 1)
+			update(1, divtree_size - 1, idx_in_divtree[node_edge_idx[b]], c, 1);
 	}
-
-	printf("query: %.3lf\n", quer);
-	printf("update: %.3lf\n", updat);
-
-	fclose(fp);
-	fclose(fp2);
 	return 0;
 }
 /*
